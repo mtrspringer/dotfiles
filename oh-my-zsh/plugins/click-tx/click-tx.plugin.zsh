a72b1f3901a7cb5d880744f841c9a716fbb335b3
mtrspringer <mtrspringer@users.noreply.github.com>
2022-06-23 21:20:33 -0400

    click helper functions

--- oh-my-zsh/plugins/click-tx/click-tx.plugin.zsh
+++ oh-my-zsh/plugins/click-tx/click-tx.plugin.zsh
@@ -3,6 +3,7 @@
 export CLOUD_ENV=local
 export COMPOSE_DOCKER_CLI_BUILD=1
 export DOCKER_BUILDKIT=1
+export CLICK_REPOS_DIR=~/Code/click
 
 # Aliases
 
@@ -27,6 +28,7 @@ alias dkr-rmi-all='dkr rmi $(dkr images -aq)'
 alias dkr-login='aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 418480071957.dkr.ecr.us-east-1.amazonaws.com'
 alias dkr-login-155-prod='aws --profile ct155-prod ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 343335910610.dkr.ecr.us-east-1.amazonaws.com'
 alias dkr-login-152-dev='aws --profile ct152-dev ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 358341525847.dkr.ecr.us-east-1.amazonaws.com'
+alias dkr-login-sbx='aws --profile sbx ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 249738588392.dkr.ecr.us-east-1.amazonaws.com'
 alias dkrc='docker-compose'
 alias dkrc-ci='dkrc -f docker-compose-ci.yml'
 alias dkrc-sys='dkrc -f docker-compose-sys.yml'
@@ -36,6 +38,7 @@ alias dkrc-sys='dkrc -f docker-compose-sys.yml'
 alias kctl="kubectl"
 alias kctx="kubectl config use-context"
 alias kctx-curr="kubectl config current-context"
+alias pod-images="kubectl get pods --all-namespaces -o jsonpath="{..image}" | tr -s '[[:space:]]' '\n' | sort | uniq -c"
 
 ## NPM
 
@@ -51,3 +54,36 @@ aws-ec2-report() {
     --query 'Reservations[*].Instances[*].{Account:NetworkInterfaces[0].OwnerId,VPC:VpcId,Subnet:SubnetId,Name:Tags[?Key==`Name`]|[0].Value,Stage:Tags[?Key==`deployment-stage`]|[0].Value,InstanceId:InstanceId,Type:InstanceType,AZ:Placement.AvailabilityZone,PublicIP:PublicIpAddress,PrivateIP:PrivateIpAddress}' \
     --output table
 }
+
+aws-mfa-session-set() {
+  mfa_code=$1
+
+  mfa_session_creds=$(aws sts get-session-token --serial-number arn:aws:iam::418480071957:mfa/matthew_limited --token-code $mfa_code)
+
+  export AWS_ACCESS_KEY_ID=$(echo $mfa_session_creds | jq -r .Credentials.AccessKeyId)
+  export AWS_SECRET_ACCESS_KEY=$(echo $mfa_session_creds | jq -r .Credentials.SecretAccessKey)
+  export AWS_SESSION_TOKEN=$(echo $mfa_session_creds | jq -r .Credentials.SessionToken)
+
+  echo "AWS MFA Session Identity:"
+  aws sts get-caller-identity
+}
+
+aws-mfa-session-unset() {
+  unset AWS_ACCESS_KEY_ID
+  unset AWS_SECRET_ACCESS_KEY
+  unset AWS_SESSION_TOKEN
+}
+
+## Docker
+
+function dkr-rehost() {
+    source=$1
+    dest=$2
+    docker pull $source
+    docker tag $source $dest
+    docker push $dest
+}
+
+## VS Code
+
+code-click() { code $CLICK_REPOS_DIR/$1 }
